493deeb04669d6be28b68d18c0757e533993bbc7
bbrfkr <bbrfkr@gmail.com>
2023-06-14 09:24:09 +0000

    mod to use mongodb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
-class ApplicationController < ActionController::Base
-    def hello
-        render html: "Hello, ArgoCD!"
-    end
+require 'mongo'
+require 'securerandom'
 
-    def goodbye
-        render html: "goodbye!"
+class ApplicationController < ActionController::Base
+    mongoUrl = ENV['MONGODB_URL']
+    @@dbClient = Mongo::Client.new(mongoUrl)
+
+    def insert
+        collection = @@dbClient[:test]
+        record_uuid = SecureRandom.uuid
+        document = {
+            record_uuid: record_uuid
+        }
+        result = collection.insert_one(document)
+        render html: "The uuid of generated record is '#{record_uuid}'."
     end
 end
